641c04b15031a04e75bb7f853e7cd562f627c44d
Victor Brekenfeld <github@drakulix.de>
2020-04-11 19:37:02 +0200

    Allow input-sys to use newer libinput versions

--- input-sys/build.rs
+++ input-sys/build.rs
@@ -12,8 +12,26 @@ fn main() {}
 #[cfg(feature = "gen")]
 fn main() {
     // Setup bindings builder
-    let generated = bindgen::builder()
-        .header("include/libinput.1.9.0.h")
+    let mut builder = bindgen::builder();
+
+    #[cfg(feature="libinput_1_15")]
+    {
+        builder = builder.header("include/libinput.1.15.0.h");
+    }
+    #[cfg(all(feature="libinput_1_14", not(feature="libinput_1_15")))]
+    {
+        builder = builder.header("include/libinput.1.14.0.h");
+    }
+    #[cfg(all(feature="libinput_1_11", not(any(feature="libinput_1_14", feature="libinput_1_15"))))]
+    {
+        builder = builder.header("include/libinput.1.11.0.h");
+    }
+    #[cfg(not(any(feature="libinput_1_11", feature="libinput_1_14", feature="libinput_1_15")))]
+    {
+        builder = builder.header("include/libinput.1.9.0.h");
+    }
+
+    let generated = builder
         .ctypes_prefix("::libc")
         .whitelist_type(r"^libinput_.*$")
         .whitelist_function(r"^libinput_.*$")
@@ -21,7 +39,7 @@ fn main() {
         .generate()
         .unwrap();
 
-    println!("cargo:rerun-if-changed=include/libinput.1.9.0.h");
+    println!("cargo:rerun-if-changed=include/");
 
     // Generate the bindings
     let out_dir = env::var("OUT_DIR").unwrap();
--- input-sys/src/lib.rs
+++ input-sys/src/lib.rs
@@ -5,8 +5,14 @@ extern crate libc;
 #[cfg(feature = "gen")]
 include!(concat!(env!("OUT_DIR"), "/gen.rs"));
 
-#[cfg(not(feature = "gen"))]
-include!("gen.rs");
+#[cfg(all(not(feature = "gen"), feature="libinput_1_15"))]
+include!("gen_1_15.rs");
+#[cfg(all(not(feature = "gen"), feature="libinput_1_14", not(feature="libinput_1_15")))]
+include!("gen_1_14.rs");
+#[cfg(all(not(feature = "gen"), feature="libinput_1_11", not(any(feature="libinput_1_14", feature="libinput_1_15"))))]
+include!("gen_1_11.rs");
+#[cfg(all(not(feature = "gen"), not(any(feature="libinput_1_11", feature="libinput_1_14", feature="libinput_1_15"))))]
+include!("gen_1_9.rs");
 
 #[link(name = "input")]
 extern {}
